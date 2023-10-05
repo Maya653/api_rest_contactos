45e4cda4e0d285f77d13bdac686853a40b955d72
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-10-05 16:53:22 +0000

    get de csv

--- API/main.py
+++ API/main.py
@@ -1,11 +1,24 @@
+import csv
+import json
 from fastapi import FastAPI
 from fastapi import status
+from fastapi.responses import JSONResponse
 
-app= FastAPI()
+
+app = FastAPI()
 
 @app.get("/",
          status_code=status.HTTP_200_OK,
-         summary="Enpoint raíz"
-)
+         summary="Enpoint raíz")
 async def root():
     return {"Hello": "World"}
+
+
+@app.get("/v1/contactos")   
+async def contactos():
+    with open("contactos.csv", mode="r", newline="") as file:
+        csv_reader = csv.DictReader(file)
+        contactos = list(csv_reader)
+        contactos_json = json.dumps(contactos)
+        response = JSONResponse(content=contactos_json)
+    return response
