e643fef50c78ec6bd1d9c260a0099506c3998401
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-10-11 16:44:09 +0000

    actu

--- API/pruebas/sandbox/imagen11/app.py
+++ API/pruebas/sandbox/imagen11/app.py
@@ -1,5 +1,15 @@
 from PIL import Image
-im = Image.open("imagen2.jpg")
-im.show()
+im = Image.open("radio.jpg")
 
 print(im.format, im.size, im.mode)
+
+box = (100, 100, 400, 400)
+region = im.crop(box)
+region.save ("recorte.jpg")
+
+r, g, b = im.split()
+im = Image.merge("RGB", (b, g, r))
+region.save("cambio.jpg")
+
+out = im.rotate(45)
+out.save("giro.jpg")
